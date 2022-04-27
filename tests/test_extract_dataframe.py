711b88afc5796f43dd015cb33c6d047f2fad568b
wakura-mbuya <wakura9@gmail.com>
2022-04-27 11:52:25 +0300

    This fixes #5
    Added unit test for testing extract_dataframe.py
    Unit tests for the functions in extract_dataframe.py

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.abspath(os.path.join('../..')))
 from extract_dataframe import read_json
 from extract_dataframe import TweetDfExtractor
 
-_, tweet_list = read_json("data/covid19.json")
+_, tweet_list = read_json("data/Economic_Twitter_Data.json")
 
 columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
     'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
@@ -29,50 +29,58 @@ class TestTweetDfExtractor(unittest.TestCase):
 
 
     def test_find_statuses_count(self):
-        self.assertEqual(self.df.find_statuses_count(), [204051, 3462, 6727, 45477, 277957])
+        self.assertEqual(self.df.find_statuses_count(), [40, 40, 40, 40, 40])
 
     def test_find_full_text(self):
-        text = ['🚨Africa is "in the midst of a full-blown third wave" of coronavirus, the head of @WHOAFRO has warned\n\nCases have risen across the continent by more than 20% and deaths have also risen by 15% in the last week\n\n@jriggers reports ~ 🧵\nhttps://t.co/CRDhqPHFWM', 'Dr Moeti is head of WHO in Africa, and one of the best public health experts and leaders I know. Hers is a desperate request for vaccines to Africa. We plead with Germany and the UK to lift patent restrictions and urgently transfer technology to enable production in Africa. https://t.co/sOgIroihOc', "Thank you @research2note for creating this amazing campaign &amp; turning social media #red4research today. @NHSRDFORUM is all about sharing the talent, passion  &amp; commitment of individuals coming together as a community for the benefit of all. You've done this. Well done 👋", 'Former Pfizer VP and Virologist, Dr. Michael Yeadon, is one of the most credentialed medical professionals speaking out about the dangers of the #Covid19 vaccines, breaks down his “list of lies” that keeps him up at night. https://t.co/LSE8CrKdqn', 'I think it’s important that we don’t sell COVAX short. It still has a lot going for it and is innovative in its design. But it needs more vaccines to share.  We’re hoping our low cost @TexasChildrens recombinant protein COVID19 vaccine with @biological_e will help fill some gaps']
+        text = ['RT @nikitheblogger: Irre: Annalena Baerbock sagt, es bricht ihr das Herz, dass man nicht bedingungslos schwere Waffen liefert.\nMir bricht e…',
+ 'RT @sagt_mit: Merkel schaffte es in 1 Jahr 1 Million "Flüchtlinge" durchzufüttern, jedoch nicht nach 16 Jahren 1 Million Rentner aus der Ar…',
+ 'RT @Kryptonoun: @WRi007 Pharma in Lebensmitteln, Trinkwasser, in der Luft oder in der Zahnpasta irgendwo muss ein Beruhigungsmittel bzw. Be…',
+ 'RT @WRi007: Die #Deutschen sind ein braves Volk!. Mit #Spritpreisen von 2 Euro abgefunden. Mit #inflation abgefunden. Mit höheren #Abgaben…',
+ 'RT @RolandTichy: Baerbock verkündet mal so nebenhin in Riga das Ende der Energieimporte aus Russland. Habeck rudert schon zurück, Scholz sc…']
 
 
         self.assertEqual(self.df.find_full_text(), text)
 
     def test_find_sentiments(self):
-        self.assertEqual(self.df.find_sentiments(self.df.find_full_text()), ([0.16666666666666666, 0.13333333333333333, 0.3166666666666667, 0.08611111111111111, 0.27999999999999997], [0.18888888888888888, 0.45555555555555555, 0.48333333333333334, 0.19722222222222224, 0.6199999999999999]))
+        self.assertEqual(self.df.find_sentiments(self.df.find_full_text()), ([0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0]))
 
     def test_find_created_time(self):
-        created_at = ['Fri Jun 18 17:55:49 +0000 2021', 'Fri Jun 18 17:55:59 +0000 2021', 'Fri Jun 18 17:56:07 +0000 2021',
-         'Fri Jun 18 17:56:10 +0000 2021', 'Fri Jun 18 17:56:20 +0000 2021']
+        created_at = ['Fri Apr 22 22:20:18 +0000 2022',
+                      'Fri Apr 22 22:19:16 +0000 2022',
+                      'Fri Apr 22 22:17:28 +0000 2022',
+                      'Fri Apr 22 22:17:20 +0000 2022',
+                      'Fri Apr 22 22:13:15 +0000 2022']
 
         self.assertEqual(self.df.find_created_time(), created_at)
 
     def test_find_source(self):
-        source = ['<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>', '<a href="https://mobile.twitter.com" rel="nofollow">Twitter Web App</a>', 
-        '<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>', '<a href="https://mobile.twitter.com" rel="nofollow">Twitter Web App</a>',
-         '<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>']
-
+        source = ['<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>', 
+                  '<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>',
+                  '<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>',
+                  '<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>',
+                  '<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>']
         self.assertEqual(self.df.find_source(), source)
 
     def test_find_screen_name(self):
-        name = ['ketuesriche', 'Grid1949', 'LeeTomlinson8', 'RIPNY08', 'pash22']
+        name = ['McMc74078966','McMc74078966','McMc74078966','McMc74078966','McMc74078966']
         self.assertEqual(self.df.find_screen_name(), name)
 
     def test_find_followers_count(self):
-        f_count = [551, 66, 1195, 2666, 28250]
+        f_count = [3, 3, 3, 3, 3]
         self.assertEqual(self.df.find_followers_count(), f_count)
 
     def test_find_friends_count(self):
-        friends_count = [351, 92, 1176, 2704, 30819]
+        friends_count = [12, 12, 12, 12, 12]
         self.assertEqual(self.df.find_friends_count(), friends_count)
 
     def test_find_is_sensitive(self):
         self.assertEqual(self.df.is_sensitive(), [None, None, None, None, None])
 
     def test_find_favourite_count(self):
-        self.assertEqual(self.df.find_favourite_count(), [548, 195, 2, 1580, 72])
+        self.assertEqual(self.df.find_favourite_count(), [113, 113, 113, 113, 113])
 
     def test_find_retweet_count(self):
-        self.assertEqual(self.df.find_retweet_count(), [612, 92, 1, 899, 20])
+        self.assertEqual(self.df.find_retweet_count(), [355, 505, 4, 332, 386])
 
     # def test_find_hashtags(self):
     #     self.assertEqual(self.df.find_hashtags(), )
@@ -81,7 +89,7 @@ class TestTweetDfExtractor(unittest.TestCase):
     #     self.assertEqual(self.df.find_mentions(), )
 
     def test_find_location(self):
-        self.assertEqual(self.df.find_location(), ['Mass', 'Edinburgh, Scotland', None, None, 'United Kingdom'])
+        self.assertEqual(self.df.find_location(), ['None', 'None', 'None', 'None', 'None'])
 
 if __name__ == '__main__':
 	unittest.main()
